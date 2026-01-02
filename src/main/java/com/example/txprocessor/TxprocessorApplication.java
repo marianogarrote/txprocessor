8dbfc62e2e0c2caeaf79f033f9092d79313a7b2b
marianogarrote <mariano.garrote7@gmail.com>
2026-01-02 13:03:47 -0300

    Initialize Spring Boot app with base hexagonal structure

---
+++ src/main/java/com/example/txprocessor/TxprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.example.txprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TxprocessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/txprocessor/TxprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.txprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
